cbcda38dc2df1ce58eb60dfe3350776492555344
Luke McAuley <luke.p.mcauley@gmail.com>
2013-02-08 01:21:09 +0000

    filled out methods and fixed hp method signatures and some spelling

--- Unimon.java
+++ Unimon.java
@@ -6,24 +6,30 @@ import java.util.*;
  * alive if status ==  1
  * dead  if status == -1
  * confused   if status % 2   = 0
- * distracted if status % 6   = 0
- * sleeping   if status % 7   = 0 
+ * distracted if status % 3   = 0
+ * sleeping   if status % 5  = 0 
  */
 
 
 
 public abstract class Unimon{
+	
+	/*
+	 * MAX_INJURY_TURNS  referes to the max turns a signal attack can 
+	 * add. A unimon cound have longer after multiple attacks;
+	 */
 	private final static int MAX_SLEEP_TURNS 	= 3;
 	private final static int MAX_CONFUSE_TURNS 	= 5;
 	private final static int MAX_DISTRACT_TURNS = 5;
 	private int hp								=100;
 	private int status							= 1;
-	private int turnsUntilNotConfuse			=0;
+	private int turnsUntilNotConfused			=0;
 	private int turnsUntilNotDistracted		    =0;
 	private int turnsUntilNotAsleep			    =0;
+	private Random rand;
 	private String name;
 	private Type type;
-	private SortedSet<Attack> attacks;
+	private ArrayList<Attack> attacks;
 	
 	
 	
@@ -31,6 +37,8 @@ public abstract class Unimon{
 		this.name = name;
 		this.type = type;
 		this.status =1;
+		attacks = new ArrayList<Attack>();
+		rand = new Random();
 	}
 	
 	/*
@@ -44,7 +52,12 @@ public abstract class Unimon{
 	/*
 	 * Decreases the the health points of the unimon
 	 */
-	public void reduceHp(){
+	public void reduceHp(int amount){
+		hp -=amount;
+		if(hp<=0){
+			hp = 0;
+			status = -1;
+		}
 	}
 	
 	
@@ -52,7 +65,8 @@ public abstract class Unimon{
 	 * Increases the current health points of the unimon.
 	 * Can increase above the starting 100.
 	 */
-	public void increaseHp(){
+	public void increaseHp(int amount){
+		hp += amount;
 	}
 	
 	/*
@@ -60,6 +74,8 @@ public abstract class Unimon{
 	 * confuses the pokemon between 1 and MAX_SLEEP_TURNS
 	 */
 	public void confuse(){
+		status *= 2;
+		turnsUntilNotConfused += rand.nextInt(MAX_CONFUSE_TURNS)+1;
 	}
 	
 	/*
@@ -67,7 +83,8 @@ public abstract class Unimon{
 	 * distract the unimon for between 1 and MAX_DISTRACT_TURNS
 	 */
 	public void distract(int turns){
-		
+		status *=3;
+		turnsUntilNotDistracted += rand.nextInt(MAX_DISTRACT_TURNS)+1;	
 	}
 	
 	
@@ -76,10 +93,13 @@ public abstract class Unimon{
 	 * sleep 
 	 */
 	public void sleep(){
+		status *=5;
+		turnsUntilNotAsleep += rand.nextInt(MAX_DISTRACT_TURNS)+1;
 	}
 	
 	/*
 	 * returns the status of the unimon
+	 * see status guide above
 	 */
 	public int getStatus(){
 		return status;
@@ -88,7 +108,7 @@ public abstract class Unimon{
 	/*
 	 * returns all the attack of the unimon
 	 */
-	public SortedSet<Attack> getAttacks(){
+	public ArrayList<Attack> getAttacks(){
 		return attacks;
 	}
 	
@@ -97,11 +117,12 @@ public abstract class Unimon{
 	 * attacks the given Unimon using the given attack.
 	 */
 	public void attack(Attack att, Unimon target){
+	//ToDO
 		
 	}
 	
 	public void attack(int attackPosition, Unimon target){
-		
+	attack(attacks.get(attackPosition),target);
 	}
 	
 	/*
@@ -116,5 +137,26 @@ public abstract class Unimon{
 	 */
 	 
 	public void endOfTurn(){
+		
+		if(turnsUntilNotConfused>1){
+			turnsUntilNotConfused--;
+		}else if(turnsUntilNotConfused==1){
+			turnsUntilNotConfused = 0;
+			status /=2;
+		}
+		if(turnsUntilNotDistracted>1){
+			turnsUntilNotDistracted--;
+		}else if(turnsUntilNotDistracted==1){
+			turnsUntilNotDistracted = 0;
+			status /=3;
+		}
+		if(turnsUntilNotAsleep>1){
+			turnsUntilNotAsleep--;
+		}else if(turnsUntilNotAsleep==1){
+			turnsUntilNotAsleep = 0;
+			status /=5;
+		}
+		
+		
 	}
 }
