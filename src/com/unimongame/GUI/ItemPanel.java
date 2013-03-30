714bcaf24cee38a579aa1121cbce40bc1f8ed727
Basile Henry <basile.henry@hotmail.com>
2013-03-30 00:10:45 +0000

    Created ItemPanel

---
+++ src/com/unimongame/GUI/ItemPanel.java
@@ -0,0 +1,30 @@
+package com.unimongame.GUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import com.unimongame.Player;
+
+public class ItemPanel extends JPanel implements ListSelectionListener,
+ActionListener {
+
+	public ItemPanel(Player p, FightGUI parent) {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
