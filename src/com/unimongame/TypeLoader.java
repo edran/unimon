305383acead5a5d83ee98364b5982077fbfc89e9
Basile Henry <basile.henry@hotmail.com>
2013-04-01 06:25:30 +0100

    Attacks takes in account types

--- src/com/unimongame/TypeLoader.java
+++ src/com/unimongame/TypeLoader.java
@@ -7,12 +7,21 @@ public class TypeLoader {
 	public static HashMap<String,Type> load(){
 		Type student = new Type("Student");
 		Type professor = new Type("Professor");
+		Type geek = new Type("Geek");
 	
+		professor.addStrength(student);
+		professor.addWeakness(geek);
 		
+		student.addStrength(geek);
+		student.addWeakness(professor);
+		
+		geek.addStrength(professor);
+		geek.addWeakness(student);
 
 		HashMap<String,Type> typemap = new HashMap<String,Type>();
 		typemap.put("Student",student);
 		typemap.put("Professor",professor );
+		typemap.put("Geek", geek);
 		return typemap;
 		
 		
